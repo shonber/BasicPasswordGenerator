bc2e70b9093ff5bf03c6eece2b77b63c462c71fd
ShonBer <32219039+shonber@users.noreply.github.com>
2022-01-27 12:19:59 +0200

    Add files via upload

---
+++ README.md
@@ -0,0 +1,10 @@
+# *Password Generator*
+
+*Basic password generator, created by Shon Berengard (shonber).* 
+
+## Setup 
+*Download and after it run the command - python3 main.php - while being in the folder*
+
+### Dependencies
+
+*Python3: https://www.python.org/ftp/python/3.10.2/python-3.10.2-amd64.exe*
---
+++ main.py
@@ -0,0 +1,44 @@
+import random
+import string
+
+
+def isValid(password, length):
+    flag = False
+    password = password[:length]
+    if len(password) == length:
+        symbols = "!@#$%^&*()_}{[]"
+        nums = "0123456789"
+
+        lowercase_letters = [c for c in password if c.islower()]
+        uppercase_letters = [c for c in password if c.isupper()]
+        symbolsInPassword = [c for c in password if c in symbols]
+        numsInPassword = [c for c in password if c in nums]
+
+        if lowercase_letters and uppercase_letters and symbolsInPassword and numsInPassword:
+            flag = True
+
+    if flag:
+        return True
+    return False
+
+
+def createPassword():
+    password = ""
+    length = 12
+
+    for _ in range(length):
+        randomLowerletters = random.choice(string.ascii_lowercase)
+        randomUpperletters = random.choice(string.ascii_uppercase)
+
+        randomInt = random.randint(0,10)
+        randomSymbols = random.choice(random.sample("!@#$%^&*()_}{[]",len("!@#$%^&*()_}{[]")))
+
+        options = [randomSymbols,randomLowerletters,randomUpperletters ,str(randomInt)]
+
+        password += random.choice(options)
+    return (password,length)
+
+
+if isValid(createPassword()[0], createPassword()[1]):
+    print (createPassword()[0])
+
